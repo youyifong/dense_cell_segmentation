912fbfa6318ae3fab37224141d1619e542c42939
Youyi Fong <youyifong@gmail.com>
2022-05-29 15:33:20 -0700

    updates

--- kdata_youyi.py
+++ kdata_youyi.py
@@ -7,15 +7,16 @@ Note
 working dir: ~/deeplearning/kdata/
 
 To train the cellpose model (on Linux):
-python -m cellpose --train --use_gpu --dir "train"  --pretrained_model cyto2 --img_filter _img --mask_filter _masks --n_epochs 500
+python -m cellpose --train --use_gpu --dir "train2"  --pretrained_model cyto2 --img_filter _img --mask_filter _masks --n_epochs 500
 
 
-To run the newly trained cellpose model (on Linux):
-python -m cellpose --use_gpu --dir "tmp" --pretrained_model "train/models/cellpose_residual_on_style_on_concatenation_off_train_2022_05_27_17_06_52.391249"  --save_tif
-
-
-To run the pretrained version of the CellPose model:
-python -m cellpose --use_gpu --dir "test" --pretrained_model cyto2  --save_tif
+To run cellpose models (on Linux):
+# trained with cd8 part
+    python -m cellpose --use_gpu --dir "train1/test" --pretrained_model "train1/models/cellpose_residual_on_style_on_concatenation_off_train1_2022_05_29_15_05_38.088182"  --save_png
+# trained with cd8 part and cd3   
+    python -m cellpose --use_gpu --dir "train2/test" --pretrained_model "train2/models/cellpose_residual_on_style_on_concatenation_off_train2_2022_05_29_15_12_30.674416"  --save_png
+# pretrained
+    python -m cellpose --use_gpu --dir "pretrained" --pretrained_model cyto2  --save_png
 
 
 """
@@ -30,33 +31,10 @@ import glob
 from read_roi import read_roi_file # pip install read-roi
 from PIL import Image, ImageDraw
 
+from utils import * # this file should be in the current working directory at this point
 
 
-# From .roi files to masks file
-def roifiles2mask(roi_files, width, height):
-    files = glob.glob(roi_files) 
-    masks = Image.new('I', (width, height), 0)
-    for idx in range(len(files)):
-        print(idx)
-        mask_temp = read_roi_file(files[idx])
-        filename = files[idx].split('\\')[-1][:-4]
-        x = mask_temp[filename]['x']
-        y = mask_temp[filename]['y']
-            
-        polygon = []
-        for i in range(len(x)):
-            polygon.append((x[i], y[i]))
-        
-        ImageDraw.Draw(masks).polygon(polygon, outline=idx+1, fill=idx+1)
-        
-    masks = np.array(masks, dtype=np.uint16) # resulting masks
-    plt.imshow(masks, cmap='gray') # display ground-truth masks
-    plt.show()
-    io.imsave(os.path.split(roi_files)[0]+'_masks.tif', masks)
-    
-    outlines = utils.masks_to_outlines(masks)
-    plt.imsave(os.path.split(roi_files)[0] + "_masks_outline.tif", outlines, cmap='gray')
-
+os.chdir("../K's training data")
 
 # get image width and height
 img = io.imread('JM_Les_Pos8_CD3-gray_CD4-green_CD8-red_aligned-CD4_CD8_GTmasks-blue.tif') # image
@@ -66,28 +44,17 @@ height = img.shape[1]
 roifiles2mask("JM_Les_Pos8_CD3_RoiSet_1908/*", width, height)
 
 
-def maskfile2outline(mask_file):
-    masks = io.imread(mask_file)
-    outlines = utils.masks_to_outlines(masks)
-    plt.imsave(os.path.splitext(mask_file)[0] + "_outline.tif", outlines, cmap='gray')
-
-
-maskfile2outline('test/M872956_Position8_CD8_test_img_cp_masks_1.tif')
-maskfile2outline('test/M872956_Position8_CD8_train_img_cp_masks_1.tif')
-maskfile2outline('M872956_Position8_CD8_test_img_cp_masks_pretrained.tif')
-maskfile2outline('M872956_Position8_CD8_test_masks.png')
-maskfile2outline('../train/M872956_Position8_CD8_train_masks.png')
-maskfile2outline('M872956_Position8_CD8_train_img_cp_masks.tif')
+maskfile2outline('M872956_Position8_CD8_test_img_cp_pretrained_masks.png')
     
 
 
 pred_mat = []
-thresholds = [0.5,0.6,0.7,0.8,0.9,1.0]
+thresholds = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
 for t in thresholds:
-    labels = io.imread('test/M872956_Position8_CD8_test_masks.png')
-    y_pred = io.imread('test/M872956_Position8_CD8_test_img_cp_masks_pretrained.tif') #0.45
-    #y_pred = io.imread('test/M872956_Position8_CD8_test_img_cp_masks.tif')#0.58
-    #y_pred = io.imread('test/M872956_Position8_CD8_test_img_cp_masks_1.tif')#0.70
-    pred_vec = csi([labels], [y_pred], threshold=t, verbose=0)
+    labels = io.imread('M872956_Position8_CD8_test_masks.png')
+    #y_pred = io.imread('M872956_Position8_CD8_test_img_cp_pretrained_masks.png') #0.73, 0.32
+    #y_pred = io.imread('M872956_Position8_CD8_test_img_cp_cd8part_masks.png') #0.89 0.58
+    y_pred = io.imread('M872956_Position8_CD8_test_img_cp_cd3cd8part_masks.png') #0.92 0.63
+    pred_vec = csi_old([labels], [y_pred], threshold=t, verbose=0) 
     pred_mat.append(pred_vec)
 pred_mat
---
+++ utils.py
@@ -0,0 +1,94 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+from read_roi import read_roi_file # pip install read-roi
+from PIL import Image, ImageDraw
+from cellpose import utils, io
+
+
+def compute_iou(labels, y_pred):
+    '''
+    Compute the IoU for ground-truth mask (labels) and the predicted mask (y_pred).
+    '''
+    true_objects = (np.unique(labels))
+    pred_objects = (np.unique(y_pred))
+    
+    # Compute intersection between all objects
+    intersection = np.histogram2d(labels.flatten(), y_pred.flatten(), bins=(true_objects, pred_objects))[0] # compute the 2D histogram of two data samples; it returns frequency in each bin
+    
+    # Compute areas (needed for finding the union between all objects)
+    area_true = np.histogram(labels, bins=true_objects)[0]
+    area_pred = np.histogram(y_pred, bins=pred_objects)[0]
+    area_true = np.expand_dims(area_true, -1) # makes true_objects * 1
+    area_pred = np.expand_dims(area_pred, 0) # makes 1 * pred_objects
+    
+    # Compute union
+    union = area_true + area_pred - intersection
+    iou = intersection / union
+    return iou[1:, 1:] # exclude background; remove frequency for bin [0,1)
+
+# Precision
+def precision_at(threshold, iou):
+    '''
+    Computes the precision at a given threshold
+    '''
+    matches = iou >= threshold
+    true_positives = np.sum(matches, axis=1) >= 1 # correct objects
+    false_positives = np.sum(matches, axis=1) == 0 # missed objects
+    false_negatives = np.sum(matches, axis=0) == 0 # extra objects
+    tp, fp, fn = (np.sum(true_positives), np.sum(false_positives), np.sum(false_negatives))
+    return tp, fp, fn
+
+# IoU
+def csi_old(truths, preds, threshold=0.5, verbose=0):
+    '''
+    Computes IoU at a given threshold
+    '''
+    ious = [compute_iou(truth, pred) for truth, pred in zip(truths, preds)]    
+    #tps, fps, fns = 0, 0, 0
+    ps=[]
+    for iou in ious:
+        tp, fp, fn = precision_at(threshold, iou)
+        #tps += tp
+        #fps += fp
+        #fns += fn
+        p = tp / (tp + fp + fn) 
+        ps.append(p)
+    p=np.mean(ps)
+    #if verbose:
+    #    print("{:1.3f}\t{}\t{}\t{}\t{:1.3f}".format(threshold, tps, fps, fns, p))        
+    return p
+
+
+# From .roi files to masks file
+def roifiles2mask(roi_files, width, height):
+    files = glob.glob(roi_files) 
+    masks = Image.new('I', (width, height), 0)
+    for idx in range(len(files)):
+        print(idx)
+        mask_temp = read_roi_file(files[idx])
+        filename = files[idx].split('\\')[-1][:-4]
+        x = mask_temp[filename]['x']
+        y = mask_temp[filename]['y']
+            
+        polygon = []
+        for i in range(len(x)):
+            polygon.append((x[i], y[i]))
+        
+        ImageDraw.Draw(masks).polygon(polygon, outline=idx+1, fill=idx+1)
+        
+    masks = np.array(masks, dtype=np.uint16) # resulting masks
+    plt.imshow(masks, cmap='gray') # display ground-truth masks
+    plt.show()
+    io.imsave(os.path.split(roi_files)[0]+'_masks.png', masks)
+    
+    outlines = utils.masks_to_outlines(masks)
+    plt.imsave(os.path.split(roi_files)[0] + "_masks_outline.png", outlines, cmap='gray')
+
+
+def maskfile2outline(mask_file):
+    masks = io.imread(mask_file)
+    outlines = utils.masks_to_outlines(masks)
+    plt.imsave(os.path.splitext(mask_file)[0] + "_outline.png", outlines, cmap='gray')
+
