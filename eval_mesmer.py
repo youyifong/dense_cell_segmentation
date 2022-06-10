cf6e8247d7d186f63142202024f3997d7a81ba79
Sunwoo Han <64387506+shan-stat@users.noreply.github.com>
2022-06-09 18:10:33 -0700

    Update eval_mesmer.py

--- eval_mesmer.py
+++ eval_mesmer.py
@@ -1,68 +1,68 @@
-### Evaluation ###
-### Library
+### 1. Evaluation images from K ###
+'''
+In this script, prediction using the pre-trained deepcell is done with Mesmer() and CytoplasmSegmentation().
+Refer to https://github.com/vanvalenlab/deepcell-tf/blob/master/notebooks/applications/Mesmer-Application.ipynb.
+'''
+
+# Library
 import os
 import numpy as np
 import deepcell
 import cv2
 
 
-### Import test data
-#prediction_folder = "models"
-#MODEL_DIR = os.path.join("/fh/fast/fong_y/tissuenet_1.0/mesmer", prediction_folder)
-#NPZ_DIR = "/fh/fast/fong_y/tissuenet_1.0/"
-# TissueNet data
-#npz_name = "tissuenet_v1.0_"
-#test_dict = np.load(NPZ_DIR + npz_name + "test.npz")
-#X_test, y_test = test_dict['X'], test_dict['y']
-
-### Images from K
+# Import images
 os.getcwd()
-dapi_img = cv2.imread('/home/shan/M872956_Position8_DAPI_img.tif', 0) # for grayscale image 
-cd3_img = cv2.imread('/home/shan/M872956_Position8_CD3_img.png') 
-
-X_test = np.stack((dapi_img, cd3_img[:,:,2]), axis=2) # first channel is DAPI, second channel is CD3
-
-
-### Load trained model
-from deepcell.model_zoo.panopticnet import PanopticNet
-prediction_model = PanopticNet(
-    backbone='resnet50',
-    #input_shape=X_test.shape[1:],
-    input_shape=X_test.shape,
-    norm_method='std', # not sure norm_method=None or norm_method=''std
-    num_semantic_heads=4,
-    num_semantic_classes=[1, 3, 1, 3], # inner distance, pixelwise, inner distance, pixelwise
-    location=True,  # should always be true
-    include_top=True)
-
-#model_name = npz_name + 'deep_watershed'
-#model_path = os.path.join(MODEL_DIR, '{}.h5'.format(model_name)) # load the trained model
-#prediction_model.load_weights(model_path, by_name=True) # load updated weights
+dapi_img = cv2.imread('/home/shan/kdata/M872956_Position8_DAPI_img_patch.png') # cv2.imread imports an image as BGR
+cd3_img = cv2.imread('/home/shan/kdata/M872956_Position8_CD3_img_patch.png')
+X_test = np.stack((dapi_img[:,:,0], cd3_img[:,:,0]), axis=2) # image with two channels: DAPI and CD3
+X_test = X_test.reshape((1,256,256,2))
+#X_test = cd3_img[:,:,0] # image with one channel: CD3
+#X_test = X_test.reshape((1,256,256,1))
 
 
-### Prediction on test data
-import skimage.io as io
-from timeit import default_timer
-from deepcell_toolbox.deep_watershed import deep_watershed
+# Prediction
+# DAPI + CD3
+from deepcell.applications import Mesmer
+app = Mesmer()
+masks = app.predict(X_test, image_mpp=2)
+cv2.imwrite('/home/shan/kdata/M872956_Position8_CD3_img_patch_masks_2ch_mpp2.png', masks[0])
 
-start = default_timer()
-test_images = prediction_model.predict(X_test) # four outputs for each image (maybe two for cell and two for nuclear)
-watershed_time = default_timer() - start
-print('Watershed segmentation of shape', test_images[0].shape, 'in', watershed_time, 'seconds.')
-
-masks = deep_watershed(
-    test_images,
-    min_distance=10,
-    detection_threshold=0.1,
-    distance_threshold=0.01,
-    exclude_border=False,
-    small_objects_threshold=0)
-
-group = 'test'
-for i in range(masks.shape[0]):
-    pred_mask = masks[i][:,:,0]
-    io.imsave(os.path.join(NPZ_DIR, 'images', group, 'res_mesmer_TN', group+str(i)+'_masks.tif'), pred_mask)
+# CD3 
+from deepcell.applications import CytoplasmSegmentation
+app = CytoplasmSegmentation()
+masks = app.predict(X_test, image_mpp=5)
+cv2.imwrite('/home/shan/kdata/M872956_Position8_CD3_img_patch_masks_1ch_mpp5.png', masks[0])
 
 
-masks_true_path = os.path.join('/fh/fast/fong_y/tissuenet_1.0/images/test/res_mesmer_TN/test0_masks.tif')
-masks_true = io.imread(masks_true_path)
+# Plotting
+import numpy as np
+from cellpose import utils, io
+import matplotlib.pyplot as plt
+
+cd3_img = io.imread('/home/shan/kdata/M872956_Position8_CD3_img_patch.png')
+masks = io.imread('/home/shan/kdata/M872956_Position8_CD3_img_patch_masks_2ch_mpp2.png')
+
+my_dpi = 96
+outlines = utils.masks_to_outlines(masks); outX, outY = np.nonzero(outlines)
+imgout = cd3_img.copy(); imgout[outX, outY] = np.array([255,255,255])
+fig=plt.figure(figsize=(imgout.shape[0]/my_dpi, imgout.shape[1]/my_dpi), dpi=my_dpi); plt.gca().set_axis_off(); plt.imshow(imgout)
+plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0); plt.margins(0,0)
+plt.gca().xaxis.set_major_locator(plt.NullLocator()); plt.gca().yaxis.set_major_locator(plt.NullLocator())
+fig.savefig("/home/shan/kdata/M872956_Position8_CD3_img_patch_outlines_2ch_mpp2.png", bbox_inches = 'tight', pad_inches = 0); plt.close('all')
+
+
+# Calculating CSI
+import numpy as np
+#from cellpose import utils, io
+from utils import * # this file should be in the current working directory at this point
+
+masks_true = io.imread('/home/shan/kdata/M872956_Position8_CD3_masks_patch.png')
+#masks_pred = io.imread('/home/shan/kdata/M872956_Position8_CD3_img_patch_masks_1ch_mpp05.png') # csi=0.0
+masks_pred = io.imread('/home/shan/kdata/M872956_Position8_CD3_img_patch_masks_1ch_mpp1.png') # csi=0.01
+#masks_pred = io.imread('/home/shan/kdata/M872956_Position8_CD3_img_patch_masks_1ch_mpp2.png') # csi=0.11
+#masks_pred = io.imread('/home/shan/kdata/M872956_Position8_CD3_img_patch_masks_2ch_mpp025.png') # csi=0.03
+#masks_pred = io.imread('/home/shan/kdata/M872956_Position8_CD3_img_patch_masks_2ch_mpp05.png') # csi=0.12
+#masks_pred = io.imread('/home/shan/kdata/M872956_Position8_CD3_img_patch_masks_2ch_mpp1.png') # csi=0.10
+#masks_pred = io.imread('/home/shan/kdata/M872956_Position8_CD3_img_patch_masks_2ch_mpp2.png') # csi=0.02
+csi([masks_true],[masks_pred], threshold=0.5, verbose=0)
