b98be10709c794ac3fdd28e73dcfb3b7dba9200d
Youyi Fong <youyifong@gmail.com>
2022-08-20 16:53:06 -0700

    replace $1 with $seed

--- cp_train_pred_eval_seed.sh
+++ cp_train_pred_eval_seed.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+seed=$1
 
-# This script takes an integer parameter, 0 to 2, which indicates the cuda_id
+# This script is to be run in the same folder as the training images
+# There should be testimages0, testimages1, testimages2 folders in this directory
+# It takes an integer parameter, 0 to 2, which indicates the cuda_id
 # Training will be done using that particular gpu
 # Suppose the cuda_id is 0, models will be saved under models0; prediction will use testimages0
-# Therer need to be testimages0, testimages1, testimages2 folders under training
 # Results will be appended to csi.txt, together with results from other gpus
+
 
 echo "Stage1: Training"
 # --diam_mean is the mean diameter to resize cells to during training 
 #       If starting from pretrained models, it cannot be changed from 30.0, but the value is saved to the model and used during prediction
 #       In cp2.0, the default for diam_mean is 17 for nuclear, 30 for cyto
 # --pretrained_model None for no pretrained model
-python -m cellpose --train --dir "." --patch_size 448 --no_rotate --pretrained_model cyto --n_epochs 500 --img_filter _img --mask_filter _masks --verbose --use_gpu --train_seed $1 --cuda_id $1
+python -m cellpose --train --dir "." --patch_size 448 --no_rotate --pretrained_model cyto --n_epochs 500 --img_filter _img --mask_filter _masks --verbose --use_gpu --train_seed $seed --cuda_id $seed
 
 # --pretrained_model $() finds the latest model under models; to train with cyto2, replace $() with cyto2
 # --diameter 0 is key. 
@@ -20,19 +23,19 @@ python -m cellpose --train --dir "." --patch_size 448 --no_rotate --pretrained_m
 #       This parameter does not impact training according to help
 echo "Stage2: Prediction and compute AP"
 # extra files mess up evaluation 
-rm testimages$1/*.npy testimages$1/*masks* 
+rm testimages$seed/*.npy testimages$seed/*masks* 
 # predict with newly trained model
-python -m cellpose --dir "testimages$1" --flow_threshold 0.4 --cellprob_threshold 0 --diameter 0  --pretrained_model $(find models$1 -type f -printf "%T@ %p\n" | sort -n | cut -d' ' -f 2- | tail -n 1)    --save_png --verbose --use_gpu --no_npy
+python -m cellpose --dir "testimages$seed" --flow_threshold 0.4 --cellprob_threshold 0 --diameter 0  --pretrained_model $(find models$seed -type f -printf "%T@ %p\n" | sort -n | cut -d' ' -f 2- | tail -n 1)    --save_png --verbose --use_gpu --no_npy
 # predict with pretrained_model   
-#python -m cellpose --dir "testimages$1" --pretrained_model cyto --flow_threshold 0.4 --cellprob_threshold 0 --diameter 0  --save_png --verbose --use_gpu
+#python -m cellpose --dir "testimages$seed" --pretrained_model cyto --flow_threshold 0.4 --cellprob_threshold 0 --diameter 0  --save_png --verbose --use_gpu
 # write csi and bias results to two files
-python -m syotil --action checkprediction --name testimage$1 --metric csi  |& tee -a csi.txt
-python -m syotil --action checkprediction --name testimage$1 --metric bias |& tee -a bias.txt
+python -m syotil --action checkprediction --name testimage$seed --metric csi  |& tee -a csi.txt
+python -m syotil --action checkprediction --name testimage$seed --metric bias |& tee -a bias.txt
 # extra files mess up evaluation 
-rm testimages$1/*.npy testimages$1/*masks* 
+rm testimages$seed/*.npy testimages$seed/*masks* 
     
     
-echo "Done with $1"
+echo "Done with $seed"
 
 
 #python ../../pred_processing.py 0 csi
