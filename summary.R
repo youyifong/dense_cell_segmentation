69942171d6a86f3c3f194b1c15e2ae0af755a9c7
Sunwoo Han <sunwooya@gmail.com>
2022-09-24 06:36:52 -0700

    Update summary.R

--- summary.R
+++ summary.R
@@ -28,39 +28,41 @@ get_avg_from_seeds <- function(file, header=T){
 #   create training1-training7 folders, each containing the needed training images/masks files and testimages0-testimages2 folders
 #   under images, run bash ../loop_cp_train_pred_eval.sh
 
-for (i in 1:3) {
-    labels=c("cyto","cyto2","none")
-    
-    # get AP
-    names=c("AP_test_cyto","AP_test_cyto2","AP_test_none")
-    files=paste0("csi_",labels[i],"_",ifelse(i==3,1,0):7,".txt")
-    res=sapply(files, function(x) get_avg_from_seeds(x, header=F))
-    colnames(res)=sub("csi_","",colnames(res))
-    # print table
-    res=t(rbind(res, mAP=colMeans(res))) 
-    rownames(res)="Train"%.%0:(nrow(res)-1)
-    colnames(res)=sub("L","",colnames(res))# remove L for lesion from names to be more succint
-    mytex(res, file=paste0("tables/",names[i]), digits=2, align="c")
-    assign(names[i], res)  
-              
+for (i in 1:5) {
+  labels=c("cyto","cyto2","tissuenet","livecell","none")
+  
+  # get AP
+  names=c("AP_test_cyto","AP_test_cyto2","AP_test_tissuenet","AP_test_livecell","AP_test_none")
+  files=paste0("csi_",labels[i],"_",ifelse(i==5,1,0):7,".txt")
+  res=sapply(files, function(x) get_avg_from_seeds(x, header=T))
+  colnames(res)=sub("csi_","",colnames(res))
+  # print table
+  res=t(rbind(res, mAP=colMeans(res))) 
+  rownames(res)="Train"%.%0:(nrow(res)-1)
+  colnames(res)=sub("L","",colnames(res))# remove L for lesion from names to be more succint
+  #mytex(res, file=paste0("tables/",names[i]), digits=2, align="c")
+  assign(names[i], res)
 }
 
-
-ylim=range(AP_test_cyto, AP_test_cyto2, AP_test_none)
+ylim=range(AP_test_cyto, AP_test_cyto2, AP_test_tissuenet, AP_test_livecell, AP_test_none)
 k=ncol(AP_test_cyto)
 training.size=c(423, 1450, 1082, 1620, 2255, 1458, 1818)
 cum.training.size=c(0,cumsum(training.size))
 names(training.size)=rownames(res)
 
-mypdf(mfrow=c(2,2), file="AP_over_masks")
+mypdf(mfrow=c(2,3), file="AP_over_masks")
     mymatplot(cum.training.size, AP_test_cyto,  ylab="AP", xlab="# of training masks", lwd=2, ylim=ylim, col=c(rep("lightblue",k-1),"blue"), cex=1, lty=c(2:k,1), main="Starting with Cyto",  y.intersp=.8)
     mymatplot(cum.training.size, AP_test_cyto2, ylab="AP", xlab="# of training masks", lwd=2, ylim=ylim, col=c(rep("lightgreen",k-1),"darkgreen"),       cex=1, lty=c(2:k,1), main="Starting with Cyto2", y.intersp=.8)
+    mymatplot(cum.training.size, AP_test_tissuenet,  ylab="AP", xlab="# of training masks", lwd=2, ylim=ylim, col=c(rep("lightpink",k-1),"lightpink3"), cex=1, lty=c(2:k,1), main="Starting with Tissuenet",  y.intersp=.8)
+    mymatplot(cum.training.size, AP_test_livecell, ylab="AP", xlab="# of training masks", lwd=2, ylim=ylim, col=c(rep("goldenrod1",k-1),"goldenrod4"),       cex=1, lty=c(2:k,1), main="Starting with Livecell", y.intersp=.8)
     mymatplot(cum.training.size[-1], AP_test_none,  ylab="AP", xlab="# of training masks", lwd=2, ylim=ylim, col=c(rep("mediumpurple1",k-1),"purple3"),       cex=1, lty=c(2:k,1), main="Starting with None", y.intersp=.8)
     mymatplot(cum.training.size, cbind(
         "Starting with cyto" =AP_test_cyto [,"mAP"], 
         "Starting with cyto2"=AP_test_cyto2[,"mAP"], 
+        "Starting with tissuenet" =AP_test_tissuenet [,"mAP"], 
+        "Starting with livecell"=AP_test_livecell[,"mAP"], 
         "Starting with none" =c(NA,AP_test_none [,"mAP"])),
-      ylab="mAP", xlab="# of training masks", lwd=2, col=c("blue","darkgreen","purple3"), lty=1, pch=1, ylim=ylim, y.intersp=.8, type="b")
+      ylab="mAP", xlab="# of training masks", lwd=2, col=c("blue","darkgreen","lightpink3","goldenrod4","purple3"), lty=1, pch=1, ylim=ylim, y.intersp=.8, type="b")
 dev.off()
 
 
