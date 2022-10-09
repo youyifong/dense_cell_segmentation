44a4a5367833c46eb3bc9b00f333d4e1a2294e9c
Youyi Fong <youyifong@gmail.com>
2022-10-09 14:11:40 -0700

    updates

--- deepcell_predict_youyi.py
+++ deepcell_predict_youyi.py
@@ -13,17 +13,24 @@ Created on Thu Oct  6 18:00:47 2022
 
 import numpy as np
 from skimage import io
+from matplotlib import pyplot as plt
+%matplotlib inline
+# needed to get download model files to work; otherwise will get ssl error
+import ssl 
+ssl._create_default_https_context = ssl._create_unverified_context
+import syotil
+
 #from deepcell.applications import CytoplasmSegmentation
 # a new clone does not work on 10/6/22, has to make sure not in a dir that contains deepcell/applications
 from deepcell.applications import NuclearSegmentation
 
-# needed to get download model files to work; otherwise will get ssl error
-import ssl 
-ssl._create_default_https_context = ssl._create_unverified_context
+
 
 # Load the image
-im0 = io.imread('CD8patch1.jpg')
-im=im0[:,:,0]
+im0 = io.imread('M872956_JML_Position8_CD3_img_patch256x256.png')
+im=im0[:,:,2]
+io.imshow(im)
+plt.show()
 
 # Expand image dimensions to rank 4
 im = np.expand_dims(im, axis=-1)
@@ -34,5 +41,14 @@ app = NuclearSegmentation()
 
 # create the lab
 y = app.predict(im)
+
+io.imshow(y)
+plt.show()
+
 np.unique(y)
-io.imsave('CD8patch1mask.png', y[0,:,:,0])
+
+mask_true=io.imread("M872956_JML_Position8_CD3_masks_patch256x256.png")
+
+syotil.csi(mask_true, y[0,:,:,0])
+
+#io.imsave('CD8patch1mask.png', y[0,:,:,0])
