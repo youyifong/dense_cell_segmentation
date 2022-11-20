bc5c1a71f194f7355a92ac0239d06a12207858d8
Youyi Fong <youyifong@gmail.com>
2022-11-19 21:14:45 -0800

    updates

--- images/addmasks.bat
+++ images/addmasks.bat
@@ -37,11 +37,20 @@
 #python -m syotil addmasks --imagefile M926910_CFL_Position7_CD3_test_img.png --mask2 ../predictions-cyto/M926910_CFL_Position7_CD3_test_img_cp_masks.png --saveas cyto
 #python -m syotil addmasks --imagefile M926910_CFL_Position13_CD3_test_img.png --mask2 ../predictions-cyto/M926910_CFL_Position13_CD3_test_img_cp_masks.png --saveas cyto
 
-# add train7 masks
-python -m syotil addmasks --imagefile M872956_JML_Position8_CD3_test_img.png --mask2 ../predictions-train7/M872956_JML_Position8_CD3_test_img_cp_masks.png --saveas train7
-python -m syotil addmasks --imagefile M872956_JML_Position8_CD4_test_img.png --mask2 ../predictions-train7/M872956_JML_Position8_CD4_test_img_cp_masks.png --saveas train7
-python -m syotil addmasks --imagefile M872956_JML_Position8_CD8_test_img.png --mask2 ../predictions-train7/M872956_JML_Position8_CD8_test_img_cp_masks.png --saveas train7
-python -m syotil addmasks --imagefile M872956_JML_Position9_CD3_test_img.png --mask2 ../predictions-train7/M872956_JML_Position9_CD3_test_img_cp_masks.png --saveas train7
-python -m syotil addmasks --imagefile M872956_JML_Position10_CD3_test_img.png --mask2 ../predictions-train7/M872956_JML_Position10_CD3_test_img_cp_masks.png --saveas train7
-python -m syotil addmasks --imagefile M926910_CFL_Position7_CD3_test_img.png --mask2 ../predictions-train7/M926910_CFL_Position7_CD3_test_img_cp_masks.png --saveas train7
-python -m syotil addmasks --imagefile M926910_CFL_Position13_CD3_test_img.png --mask2 ../predictions-train7/M926910_CFL_Position13_CD3_test_img_cp_masks.png --saveas train7
+## add train7 masks
+#python -m syotil addmasks --imagefile M872956_JML_Position8_CD3_test_img.png --mask2 ../predictions-train7/M872956_JML_Position8_CD3_test_img_cp_masks.png --saveas train7
+#python -m syotil addmasks --imagefile M872956_JML_Position8_CD4_test_img.png --mask2 ../predictions-train7/M872956_JML_Position8_CD4_test_img_cp_masks.png --saveas train7
+#python -m syotil addmasks --imagefile M872956_JML_Position8_CD8_test_img.png --mask2 ../predictions-train7/M872956_JML_Position8_CD8_test_img_cp_masks.png --saveas train7
+#python -m syotil addmasks --imagefile M872956_JML_Position9_CD3_test_img.png --mask2 ../predictions-train7/M872956_JML_Position9_CD3_test_img_cp_masks.png --saveas train7
+#python -m syotil addmasks --imagefile M872956_JML_Position10_CD3_test_img.png --mask2 ../predictions-train7/M872956_JML_Position10_CD3_test_img_cp_masks.png --saveas train7
+#python -m syotil addmasks --imagefile M926910_CFL_Position7_CD3_test_img.png --mask2 ../predictions-train7/M926910_CFL_Position7_CD3_test_img_cp_masks.png --saveas train7
+#python -m syotil addmasks --imagefile M926910_CFL_Position13_CD3_test_img.png --mask2 ../predictions-train7/M926910_CFL_Position13_CD3_test_img_cp_masks.png --saveas train7
+
+# add gt and train7 masks
+python -m syotil addmasks --mask1 ../test_gtmasks/M872956_JML_Position8_CD3_test_masks.png --imagefile M872956_JML_Position8_CD3_test_img.png --mask2 ../test_tnnucleartrain7masks/M872956_JML_Position8_CD3_test_img_cp_masks.png --saveas gt-tnnucleartrain7
+python -m syotil addmasks --mask1 ../test_gtmasks/M872956_JML_Position8_CD4_test_masks.png --imagefile M872956_JML_Position8_CD4_test_img.png --mask2 ../test_tnnucleartrain7masks/M872956_JML_Position8_CD4_test_img_cp_masks.png --saveas gt-tnnucleartrain7
+python -m syotil addmasks --mask1 ../test_gtmasks/M872956_JML_Position8_CD8_test_masks.png --imagefile M872956_JML_Position8_CD8_test_img.png --mask2 ../test_tnnucleartrain7masks/M872956_JML_Position8_CD8_test_img_cp_masks.png --saveas gt-tnnucleartrain7
+python -m syotil addmasks --mask1 ../test_gtmasks/M872956_JML_Position9_CD3_test_masks.png --imagefile M872956_JML_Position9_CD3_test_img.png --mask2 ../test_tnnucleartrain7masks/M872956_JML_Position9_CD3_test_img_cp_masks.png --saveas gt-tnnucleartrain7
+python -m syotil addmasks --mask1 ../test_gtmasks/M872956_JML_Position10_CD3_test_masks.png --imagefile M872956_JML_Position10_CD3_test_img.png --mask2 ../test_tnnucleartrain7masks/M872956_JML_Position10_CD3_test_img_cp_masks.png --saveas gt-tnnucleartrain7
+python -m syotil addmasks --mask1 ../test_gtmasks/M926910_CFL_Position7_CD3_test_masks.png --imagefile M926910_CFL_Position7_CD3_test_img.png --mask2 ../test_tnnucleartrain7masks/M926910_CFL_Position7_CD3_test_img_cp_masks.png --saveas gt-tnnucleartrain7
+python -m syotil addmasks --mask1 ../test_gtmasks/M926910_CFL_Position13_CD3_test_masks.png --imagefile M926910_CFL_Position13_CD3_test_img.png --mask2 ../test_tnnucleartrain7masks/M926910_CFL_Position13_CD3_test_img_cp_masks.png --saveas gt-tnnucleartrain7
