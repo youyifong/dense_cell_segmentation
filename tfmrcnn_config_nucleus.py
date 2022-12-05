5dcc9bbd0bde1be9a901dcf62965acc069a348fd
Youyi Fong <youyifong@gmail.com>
2022-12-05 09:29:00 -0800

    updates

---
+++ tfmrcnn_config_nucleus.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  4 22:03:13 2022
+
+@author: Youyi
+"""
+
+from mrcnn.config import Config
+import numpy as np
+
+############################################################
+#  Configurations
+############################################################
+
+class NucleusConfig(Config):
+    """Configuration for training on the nucleus segmentation dataset."""
+    # Give the configuration a recognizable name
+    NAME = "CellSeg"
+
+    # NUMBER OF worker CPUs to use
+    CPU_COUNT = 10
+    
+    # Adjust depending on your GPU memory
+    IMAGES_PER_GPU = 6
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 1  # Background + nucleus
+
+    # Number of training and validation steps per epoch
+    STEPS_PER_EPOCH = (670 - 25) // IMAGES_PER_GPU
+    VALIDATION_STEPS = max(1, 25 // IMAGES_PER_GPU)
+
+    # Don't exclude based on confidence. Since we have two classes
+    # then 0.5 is the minimum anyway as it picks between nucleus and BG
+    DETECTION_MIN_CONFIDENCE = 0
+
+    # Backbone network architecture
+    # Supported values are: resnet50, resnet101
+    # BACKBONE = "resnet50"
+    BACKBONE = "resnet101" # modified based on reverse engineered CellSeg
+
+    # Input image resizing
+    # Random crops of size 512x512
+    IMAGE_RESIZE_MODE = "crop"
+    IMAGE_MIN_DIM = 512
+    IMAGE_MAX_DIM = 512
+    IMAGE_MIN_SCALE = False
+
+    # Length of square anchor side in pixels
+    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)
+
+    # ROIs kept after non-maximum supression (training and inference)
+    POST_NMS_ROIS_TRAINING = 2000
+    POST_NMS_ROIS_INFERENCE = 6000
+
+    # Non-max suppression threshold to filter RPN proposals.
+    # You can increase this during training to generate more propsals.
+    RPN_NMS_THRESHOLD = 0.8
+
+    # How many anchors per image to use for RPN training
+    RPN_TRAIN_ANCHORS_PER_IMAGE = 256
+
+    # Image mean (RGB)
+    # MEAN_PIXEL = np.array([43.53, 39.56, 48.22])
+    MEAN_PIXEL = np.array([123.7, 116.8, 103.9]) # modified based on reverse engineered CellSeg
+
+    # If enabled, resizes instance masks to a smaller size to reduce
+    # memory load. Recommended when using high-resolution images.
+    USE_MINI_MASK = True
+    MINI_MASK_SHAPE = (56, 56)  # (height, width) of the mini-mask
+
+    # Number of ROIs per image to feed to classifier/mask heads
+    # The Mask RCNN paper uses 512 but often the RPN doesn't generate
+    # enough positive proposals to fill this and keep a positive:negative
+    # ratio of 1:3. You can increase the number of proposals by adjusting
+    # the RPN NMS threshold.
+    TRAIN_ROIS_PER_IMAGE = 200
+
+    # Maximum number of ground truth instances to use in one image
+    MAX_GT_INSTANCES = 200
+
+    # Max number of final detections per image
+    DETECTION_MAX_INSTANCES = 2000
+
+
+class NucleusInferenceConfig(NucleusConfig):
+    # Set batch size to 1 to run one image at a time
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+    # Don't resize imager for inferencing
+    IMAGE_RESIZE_MODE = "pad64"
+    # Non-max suppression threshold to filter RPN proposals.
+    # You can increase this during training to generate more propsals.
+    RPN_NMS_THRESHOLD = 0.7
