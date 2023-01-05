847c563a5102aed0c1619921b47dff2e73795faf
Youyi Fong <youyifong@gmail.com>
2023-01-04 21:10:32 -0800

    add files

--- images/postprocessing.bat
+++ images/postprocessing.bat
@@ -1,4 +1,6 @@
-## add cyto masks with tp yellow
+## run under images
+
+## add prediction masks with tp yellow
 python -m syotil colortp --mask1 test_gtmasks/M872956_JML_Position8_CD3_test_masks.png --imagefile test_images/M872956_JML_Position8_CD3_test_img.png --mask2 test_cytomasks/M872956_JML_Position8_CD3_test_img_cp_masks.png --saveas  cyto_tp
 python -m syotil colortp --mask1 test_gtmasks/M872956_JML_Position8_CD4_test_masks.png --imagefile test_images/M872956_JML_Position8_CD4_test_img.png --mask2 test_cytomasks/M872956_JML_Position8_CD4_test_img_cp_masks.png --saveas  cyto_tp
 python -m syotil colortp --mask1 test_gtmasks/M872956_JML_Position8_CD8_test_masks.png --imagefile test_images/M872956_JML_Position8_CD8_test_img.png --mask2 test_cytomasks/M872956_JML_Position8_CD8_test_img_cp_masks.png --saveas  cyto_tp
@@ -7,14 +9,32 @@ python -m syotil colortp --mask1 test_gtmasks/M872956_JML_Position10_CD3_test_ma
 python -m syotil colortp --mask1 test_gtmasks/M926910_CFL_Position7_CD3_test_masks.png --imagefile test_images/M926910_CFL_Position7_CD3_test_img.png --mask2 test_cytomasks/M926910_CFL_Position7_CD3_test_img_cp_masks.png --saveas  cyto_tp
 python -m syotil colortp --mask1 test_gtmasks/M926910_CFL_Position13_CD3_test_masks.png --imagefile test_images/M926910_CFL_Position13_CD3_test_img.png --mask2 test_cytomasks/M926910_CFL_Position13_CD3_test_img_cp_masks.png --saveas  cyto_tp
 
-## add train7 masks with tp yellow
-python -m syotil colortp --mask1 test_gtmasks/M872956_JML_Position8_CD3_test_masks.png --imagefile test_images/M872956_JML_Position8_CD3_test_img.png --mask2 test_cytotrain7masks/M872956_JML_Position8_CD3_test_img_cp_masks.png --saveas  train7_tp
-python -m syotil colortp --mask1 test_gtmasks/M872956_JML_Position8_CD4_test_masks.png --imagefile test_images/M872956_JML_Position8_CD4_test_img.png --mask2 test_cytotrain7masks/M872956_JML_Position8_CD4_test_img_cp_masks.png --saveas  train7_tp
-python -m syotil colortp --mask1 test_gtmasks/M872956_JML_Position8_CD8_test_masks.png --imagefile test_images/M872956_JML_Position8_CD8_test_img.png --mask2 test_cytotrain7masks/M872956_JML_Position8_CD8_test_img_cp_masks.png --saveas  train7_tp
-python -m syotil colortp --mask1 test_gtmasks/M872956_JML_Position9_CD3_test_masks.png --imagefile test_images/M872956_JML_Position9_CD3_test_img.png --mask2 test_cytotrain7masks/M872956_JML_Position9_CD3_test_img_cp_masks.png --saveas  train7_tp
-python -m syotil colortp --mask1 test_gtmasks/M872956_JML_Position10_CD3_test_masks.png --imagefile test_images/M872956_JML_Position10_CD3_test_img.png --mask2 test_cytotrain7masks/M872956_JML_Position10_CD3_test_img_cp_masks.png --saveas  train7_tp
-python -m syotil colortp --mask1 test_gtmasks/M926910_CFL_Position7_CD3_test_masks.png --imagefile test_images/M926910_CFL_Position7_CD3_test_img.png --mask2 test_cytotrain7masks/M926910_CFL_Position7_CD3_test_img_cp_masks.png --saveas  train7_tp
-python -m syotil colortp --mask1 test_gtmasks/M926910_CFL_Position13_CD3_test_masks.png --imagefile test_images/M926910_CFL_Position13_CD3_test_img.png --mask2 test_cytotrain7masks/M926910_CFL_Position13_CD3_test_img_cp_masks.png --saveas  train7_tp
+python -m syotil colortp --mask1 test_gtmasks/M872956_JML_Position8_CD3_test_masks.png --imagefile test_images/M872956_JML_Position8_CD3_test_img.png --mask2 test_cytotrain7masks/M872956_JML_Position8_CD3_test_img_cp_masks.png --saveas  cytotrain7_tp
+python -m syotil colortp --mask1 test_gtmasks/M872956_JML_Position8_CD4_test_masks.png --imagefile test_images/M872956_JML_Position8_CD4_test_img.png --mask2 test_cytotrain7masks/M872956_JML_Position8_CD4_test_img_cp_masks.png --saveas  cytotrain7_tp
+python -m syotil colortp --mask1 test_gtmasks/M872956_JML_Position8_CD8_test_masks.png --imagefile test_images/M872956_JML_Position8_CD8_test_img.png --mask2 test_cytotrain7masks/M872956_JML_Position8_CD8_test_img_cp_masks.png --saveas  cytotrain7_tp
+python -m syotil colortp --mask1 test_gtmasks/M872956_JML_Position9_CD3_test_masks.png --imagefile test_images/M872956_JML_Position9_CD3_test_img.png --mask2 test_cytotrain7masks/M872956_JML_Position9_CD3_test_img_cp_masks.png --saveas  cytotrain7_tp
+python -m syotil colortp --mask1 test_gtmasks/M872956_JML_Position10_CD3_test_masks.png --imagefile test_images/M872956_JML_Position10_CD3_test_img.png --mask2 test_cytotrain7masks/M872956_JML_Position10_CD3_test_img_cp_masks.png --saveas  cytotrain7_tp
+python -m syotil colortp --mask1 test_gtmasks/M926910_CFL_Position7_CD3_test_masks.png --imagefile test_images/M926910_CFL_Position7_CD3_test_img.png --mask2 test_cytotrain7masks/M926910_CFL_Position7_CD3_test_img_cp_masks.png --saveas  cytotrain7_tp
+python -m syotil colortp --mask1 test_gtmasks/M926910_CFL_Position13_CD3_test_masks.png --imagefile test_images/M926910_CFL_Position13_CD3_test_img.png --mask2 test_cytotrain7masks/M926910_CFL_Position13_CD3_test_img_cp_masks.png --saveas  cytotrain7_tp
+
+python -m syotil colortp --mask1 test_gtmasks/M872956_JML_Position8_CD3_test_masks.png --imagefile test_images/M872956_JML_Position8_CD3_test_img.png --mask2 test_tnnucleartrain7masks/M872956_JML_Position8_CD3_test_dc_masks.png --saveas  tnnucleartrain7_tp
+python -m syotil colortp --mask1 test_gtmasks/M872956_JML_Position8_CD4_test_masks.png --imagefile test_images/M872956_JML_Position8_CD4_test_img.png --mask2 test_tnnucleartrain7masks/M872956_JML_Position8_CD4_test_dc_masks.png --saveas  tnnucleartrain7_tp
+python -m syotil colortp --mask1 test_gtmasks/M872956_JML_Position8_CD8_test_masks.png --imagefile test_images/M872956_JML_Position8_CD8_test_img.png --mask2 test_tnnucleartrain7masks/M872956_JML_Position8_CD8_test_dc_masks.png --saveas  tnnucleartrain7_tp
+python -m syotil colortp --mask1 test_gtmasks/M872956_JML_Position9_CD3_test_masks.png --imagefile test_images/M872956_JML_Position9_CD3_test_img.png --mask2 test_tnnucleartrain7masks/M872956_JML_Position9_CD3_test_dc_masks.png --saveas  tnnucleartrain7_tp
+python -m syotil colortp --mask1 test_gtmasks/M872956_JML_Position10_CD3_test_masks.png --imagefile test_images/M872956_JML_Position10_CD3_test_img.png --mask2 test_tnnucleartrain7masks/M872956_JML_Position10_CD3_test_dc_masks.png --saveas  tnnucleartrain7_tp
+python -m syotil colortp --mask1 test_gtmasks/M926910_CFL_Position7_CD3_test_masks.png --imagefile test_images/M926910_CFL_Position7_CD3_test_img.png --mask2 test_tnnucleartrain7masks/M926910_CFL_Position7_CD3_test_dc_masks.png --saveas  tnnucleartrain7_tp
+python -m syotil colortp --mask1 test_gtmasks/M926910_CFL_Position13_CD3_test_masks.png --imagefile test_images/M926910_CFL_Position13_CD3_test_img.png --mask2 test_tnnucleartrain7masks/M926910_CFL_Position13_CD3_test_dc_masks.png --saveas  tnnucleartrain7_tp
+
+python -m syotil colortp --mask1 test_gtmasks/M872956_JML_Position8_CD3_test_masks.png --imagefile test_images/M872956_JML_Position8_CD3_test_img.png --mask2 test_ptmrcnn_1/M872956_JML_Position8_CD3_test_mrmasks.png --saveas  ptmrcnn_1_tp
+python -m syotil colortp --mask1 test_gtmasks/M872956_JML_Position8_CD4_test_masks.png --imagefile test_images/M872956_JML_Position8_CD4_test_img.png --mask2 test_ptmrcnn_1/M872956_JML_Position8_CD4_test_mrmasks.png --saveas  ptmrcnn_1_tp
+python -m syotil colortp --mask1 test_gtmasks/M872956_JML_Position8_CD8_test_masks.png --imagefile test_images/M872956_JML_Position8_CD8_test_img.png --mask2 test_ptmrcnn_1/M872956_JML_Position8_CD8_test_mrmasks.png --saveas  ptmrcnn_1_tp
+python -m syotil colortp --mask1 test_gtmasks/M872956_JML_Position9_CD3_test_masks.png --imagefile test_images/M872956_JML_Position9_CD3_test_img.png --mask2 test_ptmrcnn_1/M872956_JML_Position9_CD3_test_mrmasks.png --saveas  ptmrcnn_1_tp
+python -m syotil colortp --mask1 test_gtmasks/M872956_JML_Position10_CD3_test_masks.png --imagefile test_images/M872956_JML_Position10_CD3_test_img.png --mask2 test_ptmrcnn_1/M872956_JML_Position10_CD3_test_mrmasks.png --saveas  ptmrcnn_1_tp
+python -m syotil colortp --mask1 test_gtmasks/M926910_CFL_Position7_CD3_test_masks.png --imagefile test_images/M926910_CFL_Position7_CD3_test_img.png --mask2 test_ptmrcnn_1/M926910_CFL_Position7_CD3_test_mrmasks.png --saveas  ptmrcnn_1_tp
+python -m syotil colortp --mask1 test_gtmasks/M926910_CFL_Position13_CD3_test_masks.png --imagefile test_images/M926910_CFL_Position13_CD3_test_img.png --mask2 test_ptmrcnn_1/M926910_CFL_Position13_CD3_test_mrmasks.png --saveas  ptmrcnn_1_tp
+
+
+
 
 
 ## add gt and cyto masks
@@ -88,4 +108,28 @@ python -m syotil overlaymasks --mask1 test_gtmasks/M872956_JML_Position8_CD3_tes
 #python -m syotil checkprediction --gtfolder test_gtmasks --predfolder test_tfmrcnn_cellseg2 > APresults/csi_tfmrcnn_cellseg2.txt
 
 
-python -m syotil checkprediction --gtfolder test_gtmasks --predfolder test_cyto 
+python -m syotil checkprediction --gtfolder test_gtmasks --predfolder test_cytotrain7masks --min_size 1
+python -m syotil checkprediction --gtfolder test_gtmasks --predfolder test_cytotrain7masks --min_size 50
+python -m syotil checkprediction --gtfolder test_gtmasks --predfolder test_cytotrain7masks --min_size 100 #331 cnt, mAP .76
+python -m syotil checkprediction --gtfolder test_gtmasks --predfolder test_cytotrain7masks --min_size 150
+
+python -m syotil checkprediction --gtfolder test_gtmasks --predfolder test_cytomasks --min_size 0
+python -m syotil checkprediction --gtfolder test_gtmasks --predfolder test_cytomasks --min_size 50
+python -m syotil checkprediction --gtfolder test_gtmasks --predfolder test_cytomasks --min_size 100
+python -m syotil checkprediction --gtfolder test_gtmasks --predfolder test_cytomasks --min_size 150
+
+python -m syotil checkprediction --gtfolder test_gtmasks --predfolder test_cytotrain7masks --imgfolder test_images --min_totalintensity 1
+python -m syotil checkprediction --gtfolder test_gtmasks --predfolder test_cytotrain7masks --imgfolder test_images --min_totalintensity 2500
+python -m syotil checkprediction --gtfolder test_gtmasks --predfolder test_cytotrain7masks --imgfolder test_images --min_totalintensity 3500
+python -m syotil checkprediction --gtfolder test_gtmasks --predfolder test_cytotrain7masks --imgfolder test_images --min_totalintensity 4000 #333 cnt, mAP .77
+python -m syotil checkprediction --gtfolder test_gtmasks --predfolder test_cytotrain7masks --imgfolder test_images --min_totalintensity 5000
+python -m syotil checkprediction --gtfolder test_gtmasks --predfolder test_cytotrain7masks --imgfolder test_images --min_totalintensity 6000
+python -m syotil checkprediction --gtfolder test_gtmasks --predfolder test_cytotrain7masks --imgfolder test_images --min_totalintensity 10000
+python -m syotil checkprediction --gtfolder test_gtmasks --predfolder test_cytotrain7masks --imgfolder test_images --min_totalintensity 15000
+
+python -m syotil checkprediction --gtfolder test_gtmasks --predfolder test_cytotrain7masks --imgfolder test_images --min_avgintensity 1
+python -m syotil checkprediction --gtfolder test_gtmasks --predfolder test_cytotrain7masks --imgfolder test_images --min_avgintensity 30
+python -m syotil checkprediction --gtfolder test_gtmasks --predfolder test_cytotrain7masks --imgfolder test_images --min_avgintensity 35 # 336 cnt, mAP .73
+
+
+python -m syotil checkprediction --gtfolder test_gtmasks --predfolder test_cytotrain7masks --imgfolder test_images --min_size 100 --min_totalintensity 4000 #331, mAP .78
